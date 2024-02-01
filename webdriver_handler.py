e633398ae690aff879f4fa636ec32ba90cce1fd6
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2024-02-01 11:55:39 -0500

    webdriver

---
+++ webdriver_handler.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+
+
+def create_driver(url="http://shell.personal-order.cs.house", firefox=False, screen_size=(1400, 1400)):
+    if firefox:
+        options = webdriver.FirefoxOptions()
+    else:
+        options = webdriver.ChromeOptions()
+
+    options.add_argument("--window-size=%s,%s" % screen_size)
+    if firefox:
+        return webdriver.Remote(
+            command_executor=url,
+            options=options
+        )
+    else:
+        return webdriver.Remote(
+            command_executor=url,
+            options=options
+        )
+
+
+def create_local_driver(firefox=False, headless=False, screen_size=(3000, 3000)):
+    if firefox:
+        service = FirefoxService()
+        options = webdriver.FirefoxOptions()
+    else:
+        service = ChromeService()
+        options = webdriver.ChromeOptions()
+
+    if headless:
+        options.add_argument("--headless")
+    options.add_argument("--window-size=3000, 3000")
+    if firefox:
+        print("Downloaded Firefox driver. Starting Firefox.")
+        return webdriver.Firefox(service=service, options=options)
+    else:
+        try:
+            return webdriver.Chrome(service=service, options=options)
+        except Exception as e:
+            print(e)
+            print("Unable to use Chrome. Using Firefox instead.")
+            return webdriver.Firefox(service=service, options=options)
+
